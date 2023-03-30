080a3b320dcd6e359b0061f81ac3b54f419e76ac
Dudu <edudocross@gmail.com>
2023-03-30 14:05:51 -0300

    js addons

--- index.js
+++ index.js
@@ -1,82 +1,52 @@
-function inicialize(){
 
-let setTitulo = document.getElementById('titulo-receita')
-let setImg = document.getElementById('img-receita')
-let setDesc = document.getElementById('desc-receita')
+$(document).ready(function(){
 
-let titulos = ['Bolo de Fuba', 'Brigadeiro', 'Torta de Limão', 'Misto Quente'];
-let descricoes = [
-' Essa é a mais famosa do Brasil e também uma das mais simples, facil de se fazer e facil de comer', 
-'O brigadeiro é um doce genuinamente brasileiro. Um orgulho só!', 
-'Com apenas 5 ingredientes, você prepara uma torta de limão simples e muito saborosa para a sobremesa.', 
-'O melhor e mais rápido lanche para uma tarde cansativa ou uma manhã agitada.'
-]
+  let titulos = ['Bolo de Fub\u00e1', 'Brigadeiro', 'Torta de Lim\u00e3o', 'Misto Quente', 'Bolo de Aipim', 'Bolo de Cenoura com Chocolate', 'Mousse de Maracuj\u00e1'];
+  let descricoes = [
+    'Essa \u00e9 a mais famosa do Brasil e tamb\u00e9m uma das mais simples, facil de se fazer e facil de comer',
+    'O brigadeiro \u00e9 um doce genuinamente brasileiro. Um orgulho s\u00f3!',
+    'Com apenas 5 ingredientes, voc\u00ea prepara uma torta de lim\u00e3o simples e muito saborosa para a sobremesa.',
+    'O melhor e mais r\u00e1pido lanche para uma tarde cansativa ou uma manh\u00e3 agitada.',
+    'O bolo de mandioca \u00e9 uma iguaria da culin\u00e1ria brasileira.',
+    'O bolo de cenoura com chocolate \u00e9 um cl\u00e1ssico que combina com caf\u00e9 da manh\u00e3 ou lanche da tarde.',
+    'Bora aprender a fazer mousse de maracuj\u00e1? Delicioso, esse mousse de maracuj\u00e1 \u00e9 r\u00e1pido e bem f\u00e1cil de preparar!'
+  ]
 
-for (x = 0; x < titulos.length; x++){
-    setTitulo.innerHTML = titulos[x];
-    setImg.src = './assets/maisvista'+(x+1)+'.jpg';
-    setDesc.innerHTML = descricoes[x];
-    console.log(x);
-}
-}
+  let divMain = document.querySelector('.receitas-vistas-container');
 
-{/* <div class="receitas-vistas">
-          <div class="lista-receitas-vistas">
-            <div class="titulo-receita">
-              <p>Bolo de Fuba</p>
-            </div>
-            <div class="img-receita">
-              <img src="./assets/maisvista1.webp" alt="" />
-            </div>
-            <div class="desc-receita">
-              <p>
-                Essa é a mais famosa do Brasil e também uma das mais simples,
-                facil de se fazer e facil de comer
-              </p>
-            </div>
-          </div>
-          <div class="lista-receitas-vistas">
-            <div class="titulo-receita">
-              <p>Brigadeiro</p>
-            </div>
-            <div class="img-receita">
-              <img src="./assets/maisvista2.jpg" alt="" />
-            </div>
-            <div class="desc-receita">
-              <p>
-                O brigadeiro é um doce genuinamente brasileiro. Um orgulho só!
-              </p>
-            </div>
-          </div>
-          <div class="lista-receitas-vistas">
-            <div class="titulo-receita">
-              <p>Torta de Limão</p>
-            </div>
-            <div class="img-receita">
-              <img src="./assets/maisvista3.jpg" alt="" />
-            </div>
-            <div class="desc-receita">
-              <p>
-                Com apenas 5 ingredientes, você prepara uma torta de limão
-                simples e muito saborosa para a sobremesa.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="receitas-vistas">
-          <div class="lista-receitas-vistas">
-            <div class="titulo-receita">
-              <p>Misto Quente</p>
-            </div>
-            <div class="img-receita">
-              <img src="./assets/maisvista4.jpg" alt="" />
-            </div>
-            <div class="desc-receita">
-              <p>
-                O melhor e mais rápido lanche para uma tarde cansativa ou uma
-                manhã agitada.
-              </p>
-            </div>
-          </div>
-        </div>
- */}
+  for (x = 0; x < titulos.length; x++) {
+    let divLista = document.createElement('div');
+    divLista.classList.add('lista-receitas-vistas');
+    let divTitulo = document.createElement('div');
+    let divImage = document.createElement('div');
+    let divDescricao = document.createElement('div');
+    let titulo = document.createElement('p')
+    let image = document.createElement('img');
+    let descricao = document.createElement('p')
+
+    titulo.innerHTML = titulos[x];
+    image.src = './assets/img-receitas/maisvista'+ (x + 1) + '.jpg';
+    descricao = descricoes[x];
+
+    divTitulo.append(titulo);
+    divTitulo.classList.add('titulo-receita');
+    divImage.append(image);
+    divImage.classList.add('img-receita');
+    divDescricao.append(descricao);
+    divDescricao.classList.add('desc-receita')
+
+    divLista.append(divTitulo);
+    divLista.append(divImage);
+    divLista.append(divDescricao);
+    
+    if (x % 3 == 0) {
+      let divLinha = document.createElement('div');
+      divLinha.classList.add('receitas-vistas');
+      divMain.append(divLinha);
+      divLinha.append(divLista);
+    } else {
+      let ultimaLinha = document.querySelector('.receitas-vistas:last-of-type');
+      ultimaLinha.append(divLista);
+    }
+  }
+});
